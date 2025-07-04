96eaa27f676dac317eca0f2c88d6f7de05b42436
JelfferyDuran <JELFFERYDURAN@GMAIL.COM>
2025-07-04 00:13:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { bot } from "./bot.js";
 
 const app = express(); // или то, что вы используете
 app.use(express.json());
-// https://api.telegram.org/bot<token>/setWebhook?url=<url>
+// https://api.telegram.org/bot<AAGoLDZql3pp4zq1-BuE-WtmrftQk9-0tAA>/setWebhook?url=<url>
 // https://api.telegram.org/bot7397048375:AAFUc0nI6IQpsIgIWWW6ccU-gkgKSrLkMKQ/setWebhook?url=https://a357-91-206-178-2.ngrok-free.app/
 
 
